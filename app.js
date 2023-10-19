d57b7ab6c8ed953a038a950a4c17a2eab064067e
architsam <103815588+architsam@users.noreply.github.com>
2023-10-19 07:30:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const port = process.env.PORT || 8080;
-const customMessage = "Hello we are batch1 Group 2 and tgis is our first cicd deployment\n";
+const customMessage = "Hello we are batch1 Group 2 and tgis is our first cicd deployment";
 const customMessage = " cicd deployment 2";
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
