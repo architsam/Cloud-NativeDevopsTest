70e4395d22e3369363e58a796e89b447cfccd144
architsam <103815588+architsam@users.noreply.github.com>
2023-10-19 12:13:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const port = process.env.PORT || 8080;
-const customMessage = "Hello we are group 2 ";
+const customMessage = "Hello we are ";
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(`<h1>Hello, World!</h1><p>${customMessage}</p>`);
